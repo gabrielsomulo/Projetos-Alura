25f05bd274afdccaa1a225de963e29cce7a6d9fb
GABRIEL SOUZA <gabrieldsml1190@gmail.com>
2025-11-28 00:18:21 -0300

    Update Projeto-Funções.py

--- Alura/Estrutura de Dados/Projeto-Funções.py
+++ Alura/Estrutura de Dados/Projeto-Funções.py
@@ -1,2 +1,16 @@
 #Projeto-Funções
 
+def media (nota1, nota2, nota3):
+    calculo = (nota1+nota2+nota3)/3
+    return calculo
+
+print (media(1,2,3))
+
+notas = [1,2,3,4,5,6,7,8,9]
+
+def media_com_lista (lista):
+    calculo = sum(lista)/len(lista)
+    return calculo
+
+print(media_com_lista(notas))
+
