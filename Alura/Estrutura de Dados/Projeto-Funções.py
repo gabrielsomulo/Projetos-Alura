6f55ae2816f754752eba80baebb87e66b8a7afd3
GABRIEL SOUZA <gabrieldsml1190@gmail.com>
2025-11-30 23:31:59 -0300

    Update Projeto-Funções.py

--- Alura/Estrutura de Dados/Projeto-Funções.py
+++ Alura/Estrutura de Dados/Projeto-Funções.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+from math import *
+from random import choice, randrange
+
 #Projeto-Funções
 
 '''
@@ -43,3 +47,48 @@ lista_minimo = min(lista)
 print(f"\nA lista possui {lista_tamanho} números\n em que o maior número é {lista_maximo} \n e o menor número é {lista_minimo}.\n A soma dos valores presentes nela é igual a {lista_somada}")
 
 #2
+'''
+lista_tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def tabuada_lista (valor: int = [0]) -> list:
+    lista = lista_tabuada
+    resultado = list(map(lambda lst: lst*valor, lista))
+    return resultado
+
+print(f"\n RESULTADO: {tabuada_lista(7)} \n")
+'''
+def tabuada (valor: int = [0]) -> int:
+    for i in range(11):
+        print (f"\n{valor} X {i} = {valor*i}\n")
+
+    return True
+
+print(tabuada(9))
+
+
+#3
+lista_valores_1 = [97, 80, 94, 88, 80, 1, 16, 53, 62, 32, 24, 99]
+
+def multiplos (lst: list = [0], mult: int = [0]) -> list:
+    '''
+    Calcula os múltiplos
+    '''
+    resultado = []    
+    for i in range(len(lst)):
+        if lst[i]%mult == 0:
+            resultado.append(lst[i])
+
+    return resultado
+
+mult = multiplos(lista_valores_1, 2)
+
+print(f"\n {mult}")
+
+#4
+lista_quadrada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(f"\n {list(map(lambda lst: lst*lst, lista_quadrada))} \n")
+
+#5
+
+
+
