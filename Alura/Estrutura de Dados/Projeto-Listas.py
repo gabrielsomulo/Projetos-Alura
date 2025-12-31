0eb8089592df7dd8f030690184c69fc76b4cbd4c
GABRIEL SOUZA <gabrieldsml1190@gmail.com>
2025-12-31 01:01:45 -0300

    Update Projeto-Listas.py

--- Alura/Estrutura de Dados/Projeto-Listas.py
+++ Alura/Estrutura de Dados/Projeto-Listas.py
@@ -1,6 +1,6 @@
 from random import randint
 
-estudantes = ["João", "Maria", "José", "Cláudia", "Ana"]
+'''estudantes = ["João", "Maria", "José", "Cláudia", "Ana"]
 
 notas = [[1,2,3], [4,5,6], [7,8,9], [0,9,8], [7,6,5]]
 
@@ -34,4 +34,19 @@ print(estudantes_dados)
 candidatos = [ estudante[0] for estudante in estudantes_dados if estudante[1] >= 8]
 
 print(candidatos)
+'''
 
+lista_completa = [[('João', 'J720'), ('Maria', 'M205'), ('José', 'J371'), ('Cláudia', 'C546'), ('Ana', 'A347')],
+                  [[8.0, 9.0, 10.0], [9.0, 7.0, 6.0], [3.4, 7.0, 7.0], [5.5, 6.6, 8.0], [6.0, 10.0, 9.5]],
+                  [9.0, 7.3, 5.8, 6.7, 8.5],
+                  ['Aprovado', 'Aprovado', 'Reprovado', 'Aprovado', 'Aprovado']]
+
+coluna = ["Notas", "Media Final", "Situação"]
+
+cadastro = {coluna[i]: lista_completa[i+1] for i  in range(len(coluna))}
+
+print(cadastro)
+
+cadastro["Estudante"] = [lista_completa[0][i][0] for i in range(len(lista_completa[0]))]
+
+print(cadastro)
